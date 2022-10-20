867cd27ebe63614cafb10be3ea57e76d65c3e492
kiqueangulo <kiqueangulo@gmail.com>
2022-10-20 02:50:29 -0400

    Build to JS

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@ import express from "express";
 
 const app = express();
 
-/** The param req was not being used, thus TS was reaising an error.
- *  If I use underscore before the param, TS ignores it.
- *  I can also use only underscore, for future references.
- */
 app.get("/ping", (_req, res) => {
   console.log("Testing endpoint successful");
   res.send("pong");
