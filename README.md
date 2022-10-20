f12fab7024d62d9086df9641fa38cf3f3260ef57
kiqueangulo <kiqueangulo@gmail.com>
2022-10-20 03:07:10 -0400

    Create README and first endpoints

---
+++ README.md
@@ -0,0 +1,3 @@
+# PILOT DIARY
+
+This api helps to keep the logs of an aeroplain pilot working days.
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 
+import diaryRouter from "./routes/diaries";
+
 const app = express();
+
+app.use(express.json());
 
 app.get("/ping", (_req, res) => {
   console.log("Testing endpoint successful");
   res.send("pong");
 });
+
+app.use("/api/v1/diaries", diaryRouter);
 
 const PORT = 5050;
 
