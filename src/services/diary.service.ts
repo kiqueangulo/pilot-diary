90ae979215d87f2c9b2db35f3a1c08724ec4e288
kiqueangulo <kiqueangulo@gmail.com>
2022-10-20 04:13:52 -0400

    Try to make eslint to work

--- src/services/diary.service.ts
+++ src/services/diary.service.ts
@@ -2,11 +2,6 @@ import diaryData from "./diaries.json";
 
 import { DiaryEntry } from "../types";
 
-/** diaryData was raising an error between its weather(string) and DiaryEntry weather(specific strings).
- * This usually happens with external json where we don't control how the data is comming.
- * To solve this we tell TS not to worry about it and treat it as whatever we we say.
- * In this case "as Array<DiaryEntry>"
- */
 const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>;
 
 export const getEntries = () => diaries;
