91df3716b6a1201f11f1673227a46b9057e98d5f
kiqueangulo <kiqueangulo@gmail.com>
2022-10-20 02:44:51 -0400

    Install types of express and check comments

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ import express from "express";
 
 const app = express();
 
-app.get("/ping", (req, res) => {
+/** The param req was not being used, thus TS was reaising an error.
+ *  If I use underscore before the param, TS ignores it.
+ *  I can also use only underscore, for future references.
+ */
+app.get("/ping", (_req, res) => {
   console.log("Testing endpoint successful");
   res.send("pong");
 });
 
 const PORT = 5050;
 
-app.listen(PORT, () => console.table({ server: "ACTIVE", port: PORT }));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
