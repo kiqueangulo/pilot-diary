812f4469e22c42989cbc8b4e40b402777d7c884f
kiqueangulo <kiqueangulo@gmail.com>
2022-10-21 04:56:18 -0400

    Parse weather and visibility
    Since I switch from type to enum I had to change the filename to type.ts because enums are compiled to build and types.d is only for definitions

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,17 @@
-import { NewDiaryEntry, Weather } from "./types"
+import { NewDiaryEntry, Weather, Visibility } from "./types"
 
-const isString = (string: any): boolean =>
-  typeof string === "string" || string instanceof String ? true : false
+const isString = (param: any): boolean =>
+  typeof param === "string" || param instanceof String ? true : false
 
-const isDate = (date: any): boolean => {
-  if (!isString(date)) throw new Error("Incorrect or missing comment.")
+const isDate = (param: any): boolean => {
+  if (!isString(param)) throw new Error("Incorrect or missing comment.")
 
-  return Boolean(Date.parse(date))
+  return Boolean(Date.parse(param))
 }
+
+const isWeather = (param: any): boolean => Object.values(Weather).includes(param)
+
+const isVisibility = (param: any): boolean => Object.values(Visibility).includes(param)
 
 const parseComment = (commentFromRequest: any): string => {
   if (!isString(commentFromRequest)) throw new Error("Incorrect or missing comment.")
@@ -22,10 +26,26 @@ const parseDate = (dateFromRequest: any): string => {
   return dateFromRequest
 }
 
+const parseWeather = (weatherFromRequest: any): Weather => {
+  if (!isString(weatherFromRequest) || !isWeather(weatherFromRequest))
+    throw new Error("Incorrect or missing weather.")
+
+  return weatherFromRequest
+}
+
+const parseVisibility = (visibilityFromRequest: any): Visibility => {
+  if (!isString(visibilityFromRequest) || !isVisibility(visibilityFromRequest))
+    throw new Error("Incorrect or missing visibility.")
+
+  return visibilityFromRequest
+}
+
 const toNewDiaryEntry = (object: any): NewDiaryEntry => {
   const newEntry: NewDiaryEntry = {
     comment: parseComment(object.comment),
     date: parseDate(object.date),
+    weather: parseWeather(object.weather),
+    visibility: parseVisibility(object.visibility),
   }
 
   return newEntry
