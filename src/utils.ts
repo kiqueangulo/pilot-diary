d9ff79f8d81b4e238116ed486efee1278717a368
kiqueangulo <kiqueangulo@gmail.com>
2022-10-21 04:26:05 -0400

    Parse date

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,31 @@
-import { NewDiaryEntry } from "./types"
+import { NewDiaryEntry, Weather } from "./types"
+
+const isString = (string: any): boolean =>
+  typeof string === "string" || string instanceof String ? true : false
+
+const isDate = (date: any): boolean => {
+  if (!isString(date)) throw new Error("Incorrect or missing comment.")
+
+  return Boolean(Date.parse(date))
+}
 
 const parseComment = (commentFromRequest: any): string => {
-  if (typeof commentFromRequest !== "string")
-    throw new Error("Incorrect or missing comment.")
+  if (!isString(commentFromRequest)) throw new Error("Incorrect or missing comment.")
 
   return commentFromRequest
+}
+
+const parseDate = (dateFromRequest: any): string => {
+  if (!isString(dateFromRequest) || !isDate(dateFromRequest))
+    throw new Error("Incorrect or missing date.")
+
+  return dateFromRequest
 }
 
 const toNewDiaryEntry = (object: any): NewDiaryEntry => {
   const newEntry: NewDiaryEntry = {
     comment: parseComment(object.comment),
+    date: parseDate(object.date),
   }
 
   return newEntry
