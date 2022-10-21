ac0e1b627b1b86daa48f38dae7450dd5e38ddf93
kiqueangulo <kiqueangulo@gmail.com>
2022-10-21 03:59:00 -0400

    Validating comment field
    We have to validate each field

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,15 @@
 import { NewDiaryEntry } from "./types"
 
+const parseComment = (commentFromRequest: any): string => {
+  if (typeof commentFromRequest !== "string")
+    throw new Error("Incorrect or missing comment.")
+
+  return commentFromRequest
+}
+
 const toNewDiaryEntry = (object: any): NewDiaryEntry => {
   const newEntry: NewDiaryEntry = {
-    // There is gonna be logic here
+    comment: parseComment(object.comment),
   }
 
   return newEntry
